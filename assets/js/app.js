a2ca8602b7f65996a8871cfaa3666a52aa5d6dac
Ogundare Samuel Olamide <oluwadareysamuel@yahoo.com>
2020-03-24 19:48:14 +0100

    makeHttpRequest function created

---
+++ assets/js/app.js
@@ -0,0 +1,8 @@
+async function makeHttpRequest(query) {
+ const key = "AIzaSyCwmcFdumiaV93CEzz5tCyaexgoM5lV7Jk";
+ let url = `https://www.googleapis.com/customsearch/v1?key=${key}&cx=010994708637721124207:qesamehgdnf&q=${query}`;
+
+ const request = await fetch(url);
+
+ return request.json();
+}
