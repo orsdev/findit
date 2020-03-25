38f2325618b7d0a26f2ff042b2be1600f65f3ed6
Ogundare Samuel Olamide <oluwadareysamuel@yahoo.com>
2020-03-25 07:28:43 +0100

    user location added

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,12 +1,32 @@
-function currentYear() {
- const date = new Date();
- return date.getFullYear();
-};
+document.addEventListener('DOMContentLoaded', getUserLocation);
 
 function renderToDom() {
  const getYear = currentYear();
  const element = document.querySelector('.date');
  element.textContent = getYear;
+}
+
+function currentYear() {
+ const date = new Date();
+ return date.getFullYear();
+};
+
+function getUserLocation() {
+ fetch('http://ip-api.com/json')
+  .then(function (response) {
+   return response.json();
+  })
+  .then(function (data) {
+   const country = document.getElementById('country');
+   const region = document.getElementById('region');
+
+   country.textContent = data.country + ',';
+   region.textContent = data.regionName;
+
+  })
+  .catch(function (err) {
+   console.log(err)
+  })
 }
 
 renderToDom();
